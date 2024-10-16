f338913d55d1676a7847ea515c1de7a42e8fc36a
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2024-10-15 21:27:19 -0700

    addded catalogs dir but not functionality yet. froggy now loads the yaml and excludes common dirs and we have some warning assistance for the parsing. no actual value parsing yet but that comes next.

--- froggy.py
+++ froggy.py
@@ -17,9 +17,10 @@ ABSOLUTE: bool = False  # Default mode is to use only/all relative paths. Set th
 # path (nor should there ever be a starting slash for any relative path) and again, you should not include any
 # trailing slashes as os.path.join will handle any that might be needed and will do so in the cross-platform way.
 
+# TODO: If ABSOLUTE is True we can just compose an absolute BASE here. We could re-use the BASE var or add a var.
+BASE: str = '.'
 INPUT: str = 'input'
 OUTPUT: str = 'output'
-BASE: str = os.path.join('.', INPUT)
 TEMPLATES: str = 'templates'
 
 
@@ -44,16 +45,47 @@ class Froggy():
     def go(self) -> None:
         print('###############################################################')
         print(f"Froggy running at {time.strftime('%c')}")
-        for root, dirs, files in os.walk(BASE):
+        outfile: str = ''
+        content: str = ''
+        project: str = ''
+        title: str = ''
+        name: str = ''
+        directory: str = ''
+        description: str = ''
+        for root, dirs, files in os.walk(os.path.join(BASE, INPUT)):
             print(root, "consumes", end=" ")
             print(sum(os.path.getsize(os.path.join(root, name)) for name in files), end=" ")
             print("bytes in", len(files), "non-directory files")
             print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
-            if 'index.yaml' in files:
-                # files.remove('file-to-ignore.txt')  # ignore some things
-                print(f"An index.yaml file is present in this dir: {root}")
-            print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+            project = wrap_warn_html('----PROJECT----')
+            title = wrap_warn_html('----TITLE----')
+            name = wrap_warn_html('----NAME----')
+            directory = wrap_warn_html('----DIRECTORY----')
+            description = wrap_warn_html('----DESCRIPTION----')
+            # if 'index.yaml' in files:
+            #     # files.remove('file-to-ignore.txt')  # ignore some things
+            #     print(f"An index.yaml file is present in this dir: {root}")
+            # TODO: Read YAML file here. Wrap it in a try-catch. Don't die but issue an informative WARNING/ERROR.
+            #        Also, output a WARNNING/ERROR file in the place of the index.html we could not generate.
 
+            yamldoc: str = ''
+            if not exclude_from_yaml_parse(root):
+                with open(os.path.join(root, 'index.yaml'), 'r') as yamlfh:
+                    yamldoc = yamlfh.read()
+                print(yamldoc)
+
+            print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+            outfile = os.path.join(BASE, OUTPUT, 'index.html')
+            content = tmpl_item.render(
+                    project=project,
+                    title=title,
+                    name=name,
+                    directory=directory,
+                    description=description,
+                )
+            with open(outfile, mode="w", encoding="utf-8") as outfh:
+                outfh.write(content)
+                print(f"Writing: {outfile}")
 
             print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
 
@@ -66,10 +98,19 @@ class Froggy():
 
 # #############################################    FUNCTION DEFINITIONS    #############################################
 
-def ensure_one_trail_slash(pathstr: str) -> str:
-    pass
-    # If I use os.path, I should not need this.
-# end def ensure_one_trail_slash()  -  #
+def wrap_warn_html(instr: str) -> str:
+    return f"<DIV style='font-weight: bold; color: red;'>--{instr}--</DIV>"
+# end def wrap_warn_html()  -  #
+
+
+def exclude_from_yaml_parse(inpath: str) -> bool:
+    path_parts: tuple = os.path.split(inpath)
+    if path_parts[-1] == 'common':
+        print(f"Skipping 'common' dir, which is not involved in YAML parsing.")
+        return True
+    else:
+        return False
+# end def exclude_from_yaml_parse()  -  #
 
 
 # ###############################################    INITIALIZATION    #################################################
